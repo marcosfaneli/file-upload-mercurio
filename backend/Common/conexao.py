2de990f7113dc2e1ea2c1d4187ac945faea0ad73
Eduardo Vinicius de Melo Faneli <eduardofaneli@hotmail.com>
2019-01-19 14:30:37 -0300

    apagando pastas antigas

--- backend/Common/conexao.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-from common.config import USERNAME, HOST, DBNAME, PASSWORD
-
-
-def get_conexao():
-    path = "host='{}' dbname='{}' user='{}' password='{}'".format(HOST, DBNAME, USERNAME, PASSWORD)
-    conn = psycopg2.connect(path)
-
-    return conn
